c92461085332b5fb63803995a52603e5608137fa
caoyao <caoyao@testbird.com>
2019-08-01 14:42:02 +0800

    web环境

--- 3web/build.sh
+++ 3web/build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
   && apk upgrade --no-cache \
-  && apk add --no-cache \
-  node \
-  && rm -rf /var/cache/* \
-  && rm -rf /root/.cache/*
+  && apk add nodejs \
+  && apk add npm \
+ 
   
