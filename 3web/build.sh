b57aa16c53e7c09b16a05f0da606cd6411f342fa
caoyao <caoyao@testbird.com>
2019-09-27 14:40:53 +0800

    change yuantou

--- 3web/build.sh
+++ 3web/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
-sed -i /etc/apk/repositories \
-  && apk upgrade --no-cache \
+  apk upgrade --no-cache \
   && apk add nodejs \
   && apk add npm \
   && apk add git \
