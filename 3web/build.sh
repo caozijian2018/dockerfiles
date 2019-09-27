0520f7ca03694e890a7d1ff0c2581a2c6a707512
caoyao <caoyao@testbird.com>
2019-09-27 16:42:51 +0800

    web

--- 3web/build.sh
+++ 3web/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
-  apk upgrade --no-cache \
+sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
+  && apk upgrade --no-cache \
   && apk add nodejs \
   && apk add npm \
   && apk add git \
