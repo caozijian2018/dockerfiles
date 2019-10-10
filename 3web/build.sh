12d0aa1b7308c138a9b030ea804f9386f01f948b
caoyao <caoyao@testbird.com>
2019-10-10 14:36:50 +0800

    change node

--- 3web/build.sh
+++ 3web/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
   && apk upgrade --no-cache \
-  && apk add nodejs \
-  && apk add npm \
   && apk add git \
   
