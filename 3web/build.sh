02e4c10a93f0c63c982e476a373d90d2233af040
caoyao <caoyao@testbird.com>
2019-10-14 15:15:33 +0800

    change web

--- 3web/build.sh
+++ 3web/build.sh
@@ -3,8 +3,8 @@ sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
   && apk upgrade --no-cache \
   && apk add  --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/v3.10/main/ nodejs=10.16.3-r0 \
   && apk add npm \
-  && apk add git 
+  && apk add git \
   && apk add python\
-  && apk add  openssh --no-cache\
+  && apk add  openssh --no-cache \
   && npm set sass_binary_site=https://npm.taobao.org/mirrors/node-sass/
   
